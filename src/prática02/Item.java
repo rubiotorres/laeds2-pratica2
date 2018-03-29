fb40a0c58a1e4d1505bd278990ed5ad2e1c79653
Rubio Torres <rubiotorres15@gmail.com>
2018-03-28 23:26:54 -0300

    Pratica2 - implementada

---
+++ src/prática02/Item.java
@@ -0,0 +1,28 @@
+package prática02;
+
+/**
+ *
+ * @author rubio
+ */
+public class Item {
+
+    private int chave;
+
+    public Item(int chave) {
+        this.chave = chave;
+    }
+
+    public int compara(Item it) {
+        Item item = it;
+        if (this.chave < item.chave) {
+            return -1;
+        } else if (this.chave > item.chave) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public int getChave() {
+        return chave;
+    }
+}
